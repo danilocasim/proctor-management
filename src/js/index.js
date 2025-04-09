c7151eff48ce5c646061bf71be7d1d100fce27fe
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-04-09 15:57:47 +0800

    feat: add sheet api to retrive the data and convert to objects

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import schedule from "../schedule-data/first-sem-schedule.json" with { type: "json" };
+import { getNames } from "./sheet-api.js";
 
-console.log(schedule);
+console.log(await getNames());
+
+
+
 
